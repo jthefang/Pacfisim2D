3d5978c5205563bcfbf33578f7fc3011536088ef
James Fang <jtotheamesfang@gmail.com>
2020-12-25 18:23:48 -0600

    working tutorial video in webgl

---
+++ Assets/Scripts/UI/WebGLVideoPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class WebGLVideoPlayer : MonoBehaviour
+{
+    [SerializeField]
+    VideoPlayer videoPlayer;
+    [SerializeField]
+    string streamingAssetsVideoURL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, streamingAssetsVideoURL); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
